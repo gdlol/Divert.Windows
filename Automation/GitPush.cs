3e2efc48f2ddbf0af85939b634c537d458bf88df
gdlol <gdlol@users.noreply.github.com>
2025-11-09 16:39:44 +0000

    Fix image name.

--- Automation/GitPush.cs
+++ Automation/GitPush.cs
@@ -1,6 +1,5 @@
 using Cake.Frosting;
 using LibGit2Sharp;
-using Git = LibGit2Sharp;
 
 namespace Automation;
 
@@ -13,16 +12,15 @@ public class GitPush : FrostingTask<Context>
         string token =
             Environment.GetEnvironmentVariable(GIT_TOKEN)
             ?? throw new InvalidOperationException($"Environment variable {GIT_TOKEN} is not set.");
-        using var repo = new Git.Repository(Context.ProjectRoot);
+        using var repo = new Repository(Context.ProjectRoot);
         string currentBranch = repo.Head.FriendlyName;
 
         var remote = repo.Network.Remotes["origin"] ?? throw new InvalidOperationException();
         var pushRefSpec = $"+refs/heads/{currentBranch}:refs/heads/{currentBranch}";
         PushStatusError? error = null;
-        var options = new Git.PushOptions
+        var options = new PushOptions
         {
-            CredentialsProvider = (_, _, _) =>
-                new Git.UsernamePasswordCredentials { Username = "git", Password = token },
+            CredentialsProvider = (_, _, _) => new UsernamePasswordCredentials { Username = "git", Password = token },
             OnPushStatusError = (pushStatusErrors) => error = pushStatusErrors,
         };
         repo.Network.Push(remote, pushRefSpec, options);
